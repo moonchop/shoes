269df8fc7b3ea78c1f0651df573db3322f266286
moonative <minssu913@naver.com>
2021-09-06 00:00:16 +0900

    add navbar&3page

---
+++ src/components/Outfit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function outfit() {
+    return (
+        <div>
+            <h1>outfit</h1>
+        </div>
+    )
+}
---
+++ src/components/Shoes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Shoes() {
+    return (
+        <div>
+             <h1>shoes</h1>
+        </div>
+    )
+}
